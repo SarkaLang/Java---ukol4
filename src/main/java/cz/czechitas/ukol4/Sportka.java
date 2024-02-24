8e9390c351381456446305e069871b5f7a5f375c
SarkaLang <sarka.langy@gmail.com>
2024-02-24 15:43:43 +0100

    Úkol 4. Done.

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -8,10 +8,13 @@ import java.util.List;
  * Třída pro losování Sportky.
  */
 public class Sportka {
-    private final List<Integer> osudi = new ArrayList<>();
+    private final List<Integer> osudi = new ArrayList<>(50);
 
     public Sportka() {
         //TODO naplnit osudí čísly 1 až 49.
+       for (int i = 1; i < 50; i++ ) {
+           osudi.add(i);
+       }
     }
 
     /**
@@ -21,6 +24,7 @@ public class Sportka {
      */
     public void zamichej() {
         //TODO zamíchat osudí
+     Collections.shuffle(osudi);
     }
 
     /**
@@ -31,7 +35,7 @@ public class Sportka {
      */
     public List<Integer> dejVylosovanaCisla() {
         //TODO Vrátit seznam prvních 6 čísel z osudí.
-        return null;
+        return osudi.subList(0, 6);
     }
 
     /**
@@ -41,6 +45,6 @@ public class Sportka {
      */
     public Integer dejDodatkoveCislo() {
         //TODO Vrátit sedmé číslo z osudí.
-        return null;
+        return osudi.get(6);
     }
 }
